72d785bbbe25a8e3590c62d46015d22d642858cb
Hunter Bastian <bastianthunter@gmail.com>
2025-04-28 00:27:31 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+
+});
+
+
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // start AutoPlay
+    autoplay: {
+        delay: 5000,
+    }
+  });
+
